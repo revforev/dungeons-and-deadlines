00437116be93344b346e4cef46c0ca8640a1bcc0
revforev <dutulescu_andrei@yahoo.com>
2023-07-24 17:06:22 +0100

    Fixed p.Stats-i

--- app.js
+++ app.js
@@ -54,6 +54,8 @@ function toggleClass(elements, hide) {
 }
 
 toggleDisplayByTime('morning-start', 'afternoon-start', 'evening-start');
+toggleDisplayByTime('morning-q-i', 'afternoon-q-i', 'evening-q-i');
+toggleDisplayByTime('morning-stats-i', 'afternoon-stats-i', 'evening-stats-i');
 
 // =======================================================STATS================================================
 
@@ -140,7 +142,6 @@ let currentPhaseIndex = 0; // Initialize the current phase index to 0 (first pha
   function navigateBackward() {
 	let phases = ["innit", "Logo", "Greeting", "Q-i", "Stats-i", "Briefing", "Instructions", "Map-1", "Stage-1-X", "Stats-1-X", "Map-2", "Stage-2-X", "Stats-2-X", "Map-3", "Shop-3-X", "Stats-3-X", "Map-4", "Stage-4-X", "Stats-4-X", "Map-5", "Stage-5-X", "Stats-4-X", "Map-6", "Stage-6-X", "Stats-6-X", "Map-7", "Stage-7-X", "Stats-7-X", "Times-Up", "Wakeup", "Ending", "Postlude", "Credits"];
 
-    let currentPhaseIndex = 0; // Initialize the current phase index to 0 (first phase)
     // Get the current phase element from the DOM based on its ID
     let currentPhaseId = phases[currentPhaseIndex];
     let currentPhaseElement = document.getElementById(currentPhaseId);
@@ -166,6 +167,19 @@ let currentPhaseIndex = 0; // Initialize the current phase index to 0 (first pha
     }
   }
 
+function soloNavigator(elementId) {
+  const element = document.getElementById(elementId);
+  if (!element) {
+    console.error("Element with ID " + elementId + " not found.");
+    return;
+  }
+
+  if (element.classList.contains("hidden")) {
+    element.classList.remove("hidden");
+  } else {
+    element.classList.add("hidden");
+  }
+}
 
 // Make a function to restart the game
 
